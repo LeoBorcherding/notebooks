f2e1e99fb21eb526d3069a5abb2c5215650bd3bd
Etherll <mrmrmidessam@gmail.com>
2026-04-26 03:30:33 +0300

    Update AMD-Unsloth_Studio.py

--- python_scripts/AMD-Unsloth_Studio.py
+++ python_scripts/AMD-Unsloth_Studio.py
@@ -5,6 +5,8 @@
 
 
 # @title ↙️ Press ▶ to start 🦥 Unsloth Studio Chat for Llama-3.1 8b
+
+# Unsloth Studio
 # Copyright (C) 2024-present the Unsloth AI team. All rights reserved.
 
 # This program is free software: you can redistribute it and/or modify
